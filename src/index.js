d7f16f8a0af475d39be71273447a28306e49ebe4
feldhaus <maicon.feldhaus@gmail.com>
2019-08-12 21:21:04 +0200

    Setup dat.gui

--- src/index.js
+++ src/index.js
@@ -1,27 +1,95 @@
 import * as PIXI from 'pixi.js';
 import { Dungeon } from './dungeon.js';
 import { TILES } from './tiles';
+import dat from 'dat.GUI';
 
-const app = new PIXI.Application(800, 600);
-document.body.appendChild(app.view);
+const config = {
+  tileSize: 10,
+  cols: 80,
+  rows: 60,
+  randomSeed: '',
+  doorPadding: 1,
+  roomColsMin: 5,
+  roomColsMax: 10,
+  roomRowsMin: 5,
+  roomRowsMax: 10,
+};
 
-const graphics = new PIXI.Graphics();
-app.stage.addChild(graphics);
+let app;
+let graphics;
 
-const dungeon = new Dungeon({ cols: 80, rows: 60 });
+window.onload = function() {
+  setupGUI();
 
-for (let row = 0; row < dungeon.rows; row++) {
-  for (let col = 0; col < dungeon.cols; col++) {
-    const tile = dungeon.tiles[row][col];
-    if (tile === TILES.EMPTY) {
-      continue;
-    } else if (tile === TILES.WALL) {
-      graphics.beginFill(0xff0000);
-    } else if (tile === TILES.FLOOR) {
-      continue;
-    } else if (tile === TILES.DOOR) {
-      graphics.beginFill(0x0000ff);
+  app = new PIXI.Application({ backgroundColor: 0x223843 });
+  document.body.appendChild(app.view);
+
+  graphics = new PIXI.Graphics();
+  app.stage.addChild(graphics);
+
+  generateDungeon(config);
+};
+
+function generateDungeon(config = {}) {
+  const dungeon = new Dungeon({
+    cols: config.cols,
+    rows: config.rows,
+    randomSeed: config.randomSeed || null,
+    doorPadding: config.doorPadding,
+    rooms: {
+      cols: { min: config.roomColsMin, max: config.roomColsMax },
+      rows: { min: config.roomRowsMin, max: config.roomRowsMax },
+      maxRooms: 50,
+    },
+  });
+
+  graphics.clear();
+
+  for (let row = 0; row < dungeon.rows; row++) {
+    for (let col = 0; col < dungeon.cols; col++) {
+      const tile = dungeon.tiles[row][col];
+      if (tile === TILES.EMPTY) {
+        continue;
+      } else if (tile === TILES.WALL) {
+        graphics.beginFill(0xd77a61);
+      } else if (tile === TILES.FLOOR) {
+        graphics.beginFill(0xd8b4a0);
+      } else if (tile === TILES.DOOR) {
+        graphics.beginFill(0xdbd3d8);
+      }
+      graphics.drawRect(
+        col * config.tileSize,
+        row * config.tileSize,
+        config.tileSize,
+        config.tileSize
+      );
     }
-    graphics.drawRect(col * 10, row * 10, 10, 10);
   }
+}
+
+function setupGUI() {
+  const gui = new dat.GUI();
+  gui.add(config, 'tileSize').onFinishChange(() => generateDungeon(config));
+  gui.add(config, 'cols').onFinishChange(() => generateDungeon(config));
+  gui.add(config, 'rows').onFinishChange(() => generateDungeon(config));
+  gui.add(config, 'randomSeed').onFinishChange(() => generateDungeon(config));
+  gui.add(config, 'doorPadding').onFinishChange(() => generateDungeon(config));
+
+  const rooms = gui.addFolder('Rooms');
+  rooms
+    .add(config, 'roomColsMin')
+    .name('minCols')
+    .onFinishChange(() => generateDungeon(config));
+  rooms
+    .add(config, 'roomColsMax')
+    .name('maxCols')
+    .onFinishChange(() => generateDungeon(config));
+  rooms
+    .add(config, 'roomRowsMin')
+    .name('minRows')
+    .onFinishChange(() => generateDungeon(config));
+  rooms
+    .add(config, 'roomRowsMax')
+    .name('maxRows')
+    .onFinishChange(() => generateDungeon(config));
 }
